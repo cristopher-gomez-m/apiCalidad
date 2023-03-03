ea897b132073f08215ea5f9ecf6b544edbf8676f
cristopher-gomez-m <sonrisita011@hotmail.com>
2023-03-02 20:09:22 -0500

    añadido la matriculacion

--- src/routes/createdCourses.routes.js
+++ src/routes/createdCourses.routes.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
-import { getCourseGradingByStudentId, getCourses, getCoursesByStudentId, getCoursesByTeacherId } from "../controllers/courses.controller.js";
+import { getAllCourses, getCourseGradingByStudentId, getCourses, getCoursesByStudentId, getCoursesByTeacherId, registerInTheCourse, setAsistenciaByHorarioId, setPromedioByHorarioId } from "../controllers/courses.controller.js";
 const routerCreatedCourses = Router();
 
 routerCreatedCourses.get("/cursos/:curso", getCourses);
 routerCreatedCourses.get("/cursosPorEstudiante/:estudiante_id", getCoursesByStudentId);
 routerCreatedCourses.get("/notas_del_curso/:estudiante_id", getCourseGradingByStudentId);
 routerCreatedCourses.get("/cursosPorDocente/:docente_id", getCoursesByTeacherId);
+routerCreatedCourses.post("/curso/editarPromedio",setPromedioByHorarioId);
+routerCreatedCourses.post("/curso/editarAsistencia",setAsistenciaByHorarioId);
+routerCreatedCourses.get("/curso/AllCursos", getAllCourses);
+routerCreatedCourses.post("/matricularse", registerInTheCourse);
 export default routerCreatedCourses;
 
 
